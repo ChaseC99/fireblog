a5d168db9f573c4c019687d047f12c7c97dd43fc
Chase Carnaroli <chasecarnaroli@gmail.com>
2022-12-29 18:43:03 -0800

    Add Loader

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import Loader from '../components/Loader'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      <Link href={{
-        pathname: '/[username]',
-        query: { username: 'chase' },
-      }}>
-        <a>Chase's profile</a>
-      </Link>
+      <Loader show={true} /> 
     </div>
   )
 }
