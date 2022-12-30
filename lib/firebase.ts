1b0a6450a96fda35cb19a9c09a8bcaf92e8e4a76
Chase Carnaroli <chasecarnaroli@gmail.com>
2022-12-29 19:13:12 -0800

    Add sign in with Google

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,5 @@
-import firebase from "firebase/app";
-import { getAuth } from 'firebase/auth'
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
@@ -15,11 +15,9 @@ const firebaseConfig = {
 
 
 // Initialize Firebase
-var app;
-if (!firebase.getApps().length) {
-    app = firebase.initializeApp(firebaseConfig)
-}
+const app = initializeApp(firebaseConfig)
 
 export const auth = getAuth(app)
+export const googleAuthProvider = new GoogleAuthProvider()
 export const firestore = getFirestore(app)
 export const storage = getStorage(app)
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,7 +1,36 @@
+import { signInWithPopup } from "firebase/auth"
+import { auth, googleAuthProvider } from "../lib/firebase"
+
 export default function EnterPage({}) {
+    const user = null
+    const username = null
+    
     return (
         <main>
-            <h1>Sign Up</h1>
+            {user ?
+                !username ? <UsernameForm /> : <SignOutButton />
+                :
+                <SignInButton />
+            }
         </main>
     )
+}
+
+function SignInButton(){
+    const signInWithGoogle = async () => {
+        await signInWithPopup(auth, googleAuthProvider)
+    }
+    return (
+        <button className="btn-google" onClick={signInWithGoogle}>
+            <img src='/google.png' /> Sign in with Google
+        </button>
+    )
+}
+
+function SignOutButton(){
+    return <button onClick={() => auth.signOut()}>Sign Out</button>
+}
+
+function UsernameForm(){
+    return null
 }
